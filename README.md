2b61183feb863831a94b8f2e6b942aeba7db29b4
jltaglia <jltaglia@gmail.com>
2022-07-11 20:46:32 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# TPO FrontEnd
+CURSO FULL STACK CODO A CODO 2022  
+
+** TRABAJO PRACTICO FRONTEND **
+
+Trabajo práctico para demostración de los conocimientos 
+adquiridos durante la primera parte del curso Full Stack
+- HTML
+- CSS
+- Javascript
+
+
+Link de la página:
+
+vendotodopormudanza.netlify.app
---
+++ js/articulos.js
@@ -0,0 +1,82 @@
+var articulos = [
+                {descripcion: "Centro de Mesa",
+                    cantidad: 1,
+                    precio_oferta: 1500,
+                    codigo: "DECO1"
+                },
+                {descripcion: "Carpa para seis personas",
+                    cantidad: 1,
+                    precio_oferta: 25000,
+                    codigo: "EXTE1"
+                },
+                {descripcion: "Home Theather Sony 5.1",
+                    cantidad: 1,
+                    precio_oferta: 10000,
+                    codigo: "ELEC1"
+                },
+                {descripcion: "Paragüero/Florero de Bronce",
+                    cantidad: 1,
+                    precio_oferta: 5500,
+                    codigo: "DECO2"
+                },
+                {descripcion: "Florero / Paragüero de madera",
+                    cantidad: 1,
+                    precio_oferta: 3500,
+                    codigo: "DECO3"
+                },
+                {descripcion: "Juego comedor + 6 sillas algarrobo",
+                    cantidad: 1,
+                    precio_oferta: 60000,
+                    codigo: "MUEB1"
+                },
+                {descripcion: "Espejo decorativo marco madera",
+                    cantidad: 1,
+                    precio_oferta: 7500,
+                    codigo: "DECO4"
+                },
+                {descripcion: "Puf cuadrado en tela tapicería",
+                    cantidad: 1,
+                    precio_oferta: 6800,
+                    codigo: "MUEB2"
+                },
+                {descripcion: "Sillón de madera con almohadón",
+                    cantidad: 1,
+                    precio_oferta: 16500,
+                    codigo: "MUEB3"
+                },
+                {descripcion: "Mesa plegable aluminio p/camping",
+                    cantidad: 1,
+                    precio_oferta: 8500,
+                    codigo: "EXTE2"
+                },
+                {descripcion: "Centro de mesa madera c/césped artif.",
+                    cantidad: 1,
+                    precio_oferta: 2500,
+                    codigo: "DECO5"
+                },
+                {descripcion: "Heladera con freezer GAFA",
+                    cantidad: 1,
+                    precio_oferta: 38000,
+                    codigo: "ELEC2"
+                },
+                {descripcion: "Botinero línea Platinum",
+                    cantidad: 1,
+                    precio_oferta: 17000,
+                    codigo: "MUEB3"
+                },
+                {descripcion: "Espejo oval vintage marco madera",
+                    cantidad: 1,
+                    precio_oferta: 12800,
+                    codigo: "DECO6"
+                },
+                {descripcion: "Florero de estilo rupestre/étnico",
+                    cantidad: 1,
+                    precio_oferta: 8900,
+                    codigo: "DECO7"
+                },
+                {descripcion: "Cuadro deco tríptico sin marco",
+                    cantidad: 1,
+                    precio_oferta: 12500,
+                    codigo: "DECO8"
+                },
+            ]
